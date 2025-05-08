c283a7133342e7d9545d612c43deeae32744453c
kashc <kashc@THE_BEAST>
2025-05-08 19:05:47 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prog7311_Assignment_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
